a643942462646ff20a91576d040cd95e51bf287a
asyraf_chelah <asyraf_chelah@dell.com>
2019-09-22 02:31:50 +0800

    adding reactiveform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-bitly';
+  
 
-  // onsubmit(){
-  //   this.
-  // }
+  inputUrl = new FormControl ('')
+
+  
+
+
+  onsubmit(){
+    this.inputUrl.value
+  }
 }
